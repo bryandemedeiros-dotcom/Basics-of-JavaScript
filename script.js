2a1be43148f70bbb5a1359ac72294001ea4385ed
bryandemedeiros-dotcom <bryan.demedeiros@gmail.com>
2026-01-30 14:13:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let greeting = "Hello, JavaScript!";
+console.log("String:", greeting);
+
+let studentName = "Bryan D";
+let studentAge = 40;
+let isEnrolled = true;
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Is Enrolled:", isEnrolled);
+
+let age = 25;
+let price = 19.99;
+console.log("Number (integer):", age);
+console.log("Number (float):", price);
+
+let isLoggedIn = true;
+console.log("Boolean:", isLoggedIn);
+
+let fruits = ["apple", "banana", "cherry"];
+console.log("Array:", fruits);
+
+let person = {
+  firstName: "Bryan",
+  lastName: "D"
+};
+console.log("Object:", person);
